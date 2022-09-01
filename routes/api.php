10fedcd47a995f479db1c3375979e0443960c0f2
Yusti <oyusti@gmail.com>
2022-09-01 11:35:01 -0400

    Trabajo en api privada para la seccion profesores terminado

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,17 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\V1\TeacherController;
+use App\Http\Controllers\Api\LoginController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+
+/* Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-});
+}); */
+
+Route::apiResource('v1/teachers', TeacherController::class)
+    ->only(['index','show', 'destroy'])
+    ->middleware('auth:sanctum');
+
+Route::post('login', [LoginController::class, 'login']);
